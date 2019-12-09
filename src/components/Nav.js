b4fea3d2dac37e6cfc1ec94a9263fd72403715cc
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-12-09 01:37:21 +0000

    Added Nav Component

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+class App extends Component {
+render() {
+  return (
+    <div className="App">
+      <div>
+        Nav Component
+      </div>
+    </div>
+  );
+}
+}
+
+export default App;
