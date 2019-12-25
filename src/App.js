7664361b40758e4e8ade24338cb9def6c6f9778b
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-12-25 23:19:13 +0000

    delete notes

--- src/App.js
+++ src/App.js
@@ -17,32 +17,65 @@ class App extends Component {
   }
 
   toggleNote = () => {
-  this.setState({
-    showNote: ! this.state.showNote
-  });
-}
+    this.setState({
+      showNote: ! this.state.showNote,
+      note: {}
+    });
+  }
 
-getNotes = () => {
-  axios.get(urlFor('notes'))
-  .then((res) => this.setState({ notes: res.data }) )
-  .catch((err) => console.log(err.response.data) );
-}
+  getNotes = () => {
+    axios.get(urlFor('notes'))
+    .then((res) => this.setState({ notes: res.data }) )
+    .catch((err) => console.log(err.response.data) );
+  }
 
-getNote = (id) => {
-  axios.get(urlFor(`notes/${id}`))
-  .then((res) => this.setState({ note: res.data, showNote: true }) )
-  .catch((err) => console.log(err.response.data) );
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  performSubmissionRequest = (data, id) => {
+     if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+     this.performSubmissionRequest(data, id)
+    .then((res) => this.setState({ showNote: false }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  deleteNote = (id) => {
+    const newNotesState = this.state.notes.filter((note) => note.id !== id );
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ notes: newNotesState }))
+    .catch((err) => console.log(err.response.data) );
   }
 
 
-
   render() {
-    const { showNote, notes } = this.state;
+    const { showNote, notes, note } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List getNotes={this.getNotes} notes={notes} getNote={this.getNote} /> }
+        { showNote ? 
+          <Note
+            note={note} 
+            submitNote={this.submitNote} 
+          /> 
+          : 
+          <List 
+            getNotes={this.getNotes} 
+            notes={notes} 
+            getNote={this.getNote} 
+            deleteNote={this.deleteNote}
+          /> 
+        }
       </div>
     );
   }
